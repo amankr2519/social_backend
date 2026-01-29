401955be9429e9fef4f71bbd0fce9ea23859d12c
Aman Kumar <ak3230531@gmail.com>
2026-01-29 23:41:57 +0530

    Build api for reading posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -13,4 +13,29 @@ const createPost = async(req, res)=>{
   }
 }
 
-module.exports = {createPost};
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .populate('user', 'username email')
+      .sort({ createdAt: -1 });
+
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getMyPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({ user: req.user._id })
+      .populate('user', 'username email')
+      .sort({ createdAt: -1 });
+
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+module.exports = {createPost,getAllPosts,getMyPosts};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {createPost} = require('../controllers/postController');
+const {createPost,getAllPosts,getMyPosts} = require('../controllers/postController');
 const protect = require('../middleware/authMiddleware');
 
 router.post('/',protect,createPost);
+router.get('/',getAllPosts);
+router.get('/me',protect,getMyPosts);
 
 module.exports = router;
